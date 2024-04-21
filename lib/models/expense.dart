b974914328d000efe5cf2384eebb824e3e4eca52
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-21 04:42:13 -0500

    created enum of category

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,6 +2,10 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+// Create a fixed set of allowed values to be used in the category,
+// You have to use atleast one of these values to create a category
+enum Category { food, travel, leisure, work }
+
 class Expense {
   // Build a constructor function that can be called to build an expense object based on this blueprint
   // Use name parameter to pass in data
@@ -9,6 +13,7 @@ class Expense {
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
     // initialize uuid function
   }) : id = uuid.v4();
 
@@ -17,4 +22,5 @@ class Expense {
   final double amount;
   final String id;
   final DateTime date;
+  final Category category;
 }
