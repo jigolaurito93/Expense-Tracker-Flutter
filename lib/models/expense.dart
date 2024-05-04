b1e28bd254562a4c92b3501882b11ac2eb3a5be9
Jose Laurito <jigolaurito93@yahoo.com>
2024-05-04 02:30:34 -0500

    install intl package to format date properly

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,18 @@
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
+import 'package:flutter/material.dart';
 
 enum Category { food, travel, leisure, work }
 
+final formatter = DateFormat.yMd();
 const uuid = Uuid();
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.work: Icons.work,
+  Category.leisure: Icons.movie
+};
 
 class Expense {
   Expense(
@@ -17,4 +27,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
