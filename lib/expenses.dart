ba212fb7caebdfe668fddb59233c3e81b345462e
Jose Laurito <jigolaurito93@yahoo.com>
2024-05-04 03:39:46 -0500

    widget for inputting new expense

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import "package:expense_tracker/models/expense.dart";
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -27,7 +28,7 @@ class _ExpensesState extends State<Expenses> {
     showModalBottomSheet(
         context: context,
         builder: (ctx) {
-          return const Text('Whats are you');
+          return const NewExpense();
         });
   }
 
