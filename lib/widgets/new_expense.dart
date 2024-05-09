0b1b3604bd892d4b03aef0604baea71d587d81aa
Jose Laurito <jigolaurito93@yahoo.com>
2024-05-09 03:46:16 -0500

    Created controller for textfield input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -9,10 +9,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -25,14 +27,29 @@ class _NewExpenseState extends State<NewExpense> {
           TextField(
             controller: _titleController,
             maxLength: 50,
-            decoration: const InputDecoration(label: Text('Title')),
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              label: Text('Amount'),
+            ),
           ),
           Row(
             children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text("Cancel"),
+              ),
               ElevatedButton(
                   child: const Text('Save Expense'),
                   onPressed: () {
                     print(_titleController.text);
+                    print(_amountController.text);
                   })
             ],
           )
